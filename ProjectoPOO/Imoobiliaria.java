508bd97a4ba6b154ed847c5b85ee95dedf3458b2
Ricardo Pereira <ricardo.agp.914@gmail.com>
2016-03-19 15:43:35 +0000

    Trabalho

---
+++ ProjectoPOO/Imoobiliaria.java
@@ -0,0 +1,20 @@
+
+public class Imoobiliaria
+{
+   public static void initApp(){
+    }
+   
+   public void registarUtilizador(Utilizador utilizador) throws UtilizadorExistenteException
+   {
+    
+    }
+    
+   public void iniciaSessao(String email,String password) throws SemAutorizacaoException
+   {
+    
+    }
+   
+   public void fecharSessao(){
+    
+    }  
+}
---
+++ ProjectoPOO/Utilizador.java
@@ -0,0 +1,6 @@
+
+public class Utilizador
+{
+   private Utilizador Comprador;
+   private Utilizador Vendedor;
+}
